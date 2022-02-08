9071733910677550a8eab16d2be9641d9e0296d0
Viratsoam <soamvikas000@gmail.com>
2022-02-08 10:23:53 +0530

    basic calculator file added

---
+++ JavascriptPractice/basicCalculator.js
@@ -0,0 +1,29 @@
+const calculator = (a, b, operation) => {
+    let ans;
+    switch (operation) {
+        case '+':
+            ans = a * b;
+            break;
+        case '-':
+            ans = a - b;
+            break;
+        case '*':
+            ans = a * b;
+            break;
+        case '/':
+            ans = a / b;
+            break;
+        case '%':
+            ans = a % b;
+            break;
+        case '**':
+            ans = a ** b;
+            break;
+        default:
+            ans = "Oop's you have entered wrong operator!!";
+
+    }
+    return ans;
+}
+
+console.log(calculator(2, 3, '**'));
